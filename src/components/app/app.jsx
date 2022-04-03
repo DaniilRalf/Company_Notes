132cde7800f2b57436a7accca3c570a49e890645
Daniil <ralf.danya@mail.ru>
2022-04-04 01:16:14 +0700

    start project

---
+++ src/components/app/app.jsx
@@ -0,0 +1,13 @@
+import Info from '../info/info';
+
+import './app.css';
+
+
+function App(){
+    return(
+        <div className="app">
+            <Info />
+        </div>
+    );
+}
+export default App;
---
+++ src/components/info/info.jsx
@@ -0,0 +1,12 @@
+import './info.css';
+
+function Info(){
+    return(
+        <div className="Info">
+            <h1>Учет сотрудников  вкомпании Name</h1>
+            <h2>Общее количество сотрудников: </h2>
+            <h2>Число сотрудников получающих премиальные в текущем месяце: </h2>
+        </div>
+    );
+};
+export default Info;
